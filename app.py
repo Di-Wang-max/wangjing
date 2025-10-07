f12fc1fd0eadfba85c9477518e0854dc2d582067
Di-Wang-max <favorite20190419@gmail.com>
2025-10-07 07:21:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ Age = st.number_input("Age (Years):",
     value=18,          # 默认值（可选，默认为 min_value）
     step=1,            
     help="Must be 18-85 years")
+if Age > 85 or Age < 18:
+    st.session_state.age_valid = False
+    st.error("Value must be between 18 and 85.")
+elif not st.session_state.age_valid:
+    st.session_state.age_valid = True
+    st.success("Valid age selected.")
+else:
+    st.write(f"Selected age: {Age} years")
 D_dimer_D3 = st.number_input("Postoperative Day 3 D-dimer (μg/mL):",
    min_value=0.00,      # 最小值
     max_value=10.00,      # 最大值
